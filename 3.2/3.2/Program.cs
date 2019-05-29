e484f1ecdb14999f5d6f985f4fcd78f7dbc078ad
EugeneLazakovich <zlazakovich@gmail.com>
2019-05-29 15:57:33 +0300

    My homework3, Lazakovich

---
+++ 3.2/3.2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace _3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(0, 100);
+            Boolean answer;
+            Console.WriteLine("Enter your number #1:");
+            string stry1 = Console.ReadLine();
+            int try1 = int.Parse(stry1);
+            if (try1 == value)
+            {
+                answer = true;
+                goto theend;
+            }
+            else
+            {
+                if (try1 < value)
+                {
+                    Console.WriteLine("My number is bigger!");
+                }
+                else
+                {
+                    Console.WriteLine("My number is smaller!");
+                }
+            }
+            Console.WriteLine("Enter your number #2:");
+            string stry2 = Console.ReadLine();
+            int try2 = int.Parse(stry2);
+            if (try2 == value)
+            {
+                answer = true;
+                goto theend;
+            }
+            else
+            {
+                if (try2 < value)
+                {
+                    Console.WriteLine("My number is bigger!");
+                }
+                else
+                {
+                    Console.WriteLine("My number is smaller!");
+                }
+            }
+            Console.WriteLine("Enter your number #3:");
+            string stry3 = Console.ReadLine();
+            int try3 = int.Parse(stry3);
+            if (try3 == value)
+            {
+                answer = true;
+                goto theend;
+            }
+            else
+            {
+                if (try3 < value)
+                {
+                    Console.WriteLine("My number is bigger!");
+                }
+                else
+                {
+                    Console.WriteLine("My number is smaller!");
+                }
+            }
+            Console.WriteLine("Enter your number #4:");
+            string stry4 = Console.ReadLine();
+            int try4 = int.Parse(stry4);
+            if (try4 == value)
+            {
+                answer = true;
+                goto theend;
+            }
+            else
+            {
+                if (try4 < value)
+                {
+                    Console.WriteLine("My number is bigger!");
+                }
+                else
+                {
+                    Console.WriteLine("My number is smaller!");
+                }
+            }
+            Console.WriteLine("Enter your number #5:");
+            string stry5 = Console.ReadLine();
+            int try5 = int.Parse(stry5);
+            if (try5 == value)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            theend:
+            if (answer)
+            {
+                Console.WriteLine("Congratulations! You win!");
+            }
+            else
+            {
+                Console.WriteLine("You lose!");
+            }
+            Console.WriteLine("Correct answer: "+value);
+
+        }
+    }
+}
---
+++ 3.3/3,3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd=new Random();
+            int value = rnd.Next(0, 1000000);
+            Console.WriteLine("Random: "+value);
+            int digitCount = value;
+            int count=0;
+            for(int i=0; digitCount > 0;i++)
+            {
+                digitCount /= 10;
+                count++;
+            }
+            Console.WriteLine("Number of digits: "+count);
+            int[] numbers = new int[count+1];
+            int degree = 1;
+            for (int i=0;i<(count-1);i++)
+            {
+                degree *= 10;
+            }
+            int sum = 0;
+            int oddCount = 0;
+            for(int i=count;i>0;i--)
+            {
+                numbers[i] = value / degree;
+                value-=(numbers[i] * degree);
+                degree /= 10;
+                if(numbers[i]%3==0)
+                {
+                    sum += numbers[i];
+                }
+                if(numbers[i]%2==1)
+                {
+                    oddCount++;
+                }
+            }
+            Console.WriteLine("Summa %3: "+sum);
+            Console.WriteLine("Number of odd: " + oddCount);
+        }
+    }
+}
